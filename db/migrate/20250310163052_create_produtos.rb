ad400500858570be2abd4457ac5bb158e6270b07
root <root@BOOK-6MLF5VO0ML.>
2025-03-10 15:00:12 -0300

    Segundo commit 0.1

--- db/migrate/20250310163052_create_produtos.rb
+++
@@ -1,9 +0,0 @@
-class CreateProdutos < ActiveRecord::Migration[8.0]
-  def change
-    create_table :produtos do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_10_160549) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_10_172745) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "produtos", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
